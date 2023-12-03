1050802872c7dc1a7ce455d36bb0bfbbdea03077
Konstantin Baltsat <baltsat2002@mail.ru>
2023-12-03 13:27:37 +0100

    Signal filtering

--- server.py
+++ server.py
@@ -2,13 +2,13 @@ import socket
 import ipaddress
 import netifaces
 import numpy as np
-from scipy.signal import savgol_filter
+from scipy.signal import savgol_filter, coherence
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
 signal_history = []
-WINDOW_SIZE=5
+WINDOW_SIZE=100
 
 def get_ip_and_subnet():
     # Get the name of the default socket interface
@@ -25,14 +25,18 @@ def calculate_broadcast_address(ip, netmask):
 
 # Function to apply moving average filter to denoise the signal
 def moving_average(data, window_size=WINDOW_SIZE):
-    return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+    if len(data) < window_size:
+        return np.convolve(data, np.ones(window_size)/window_size, mode='valid')
+    else:
+        return data
+
 
 # Function to apply Savitzky-Golay filter to denoise the signal
-def savitzky_golay_filter(data, window_size=WINDOW_SIZE, order=3):
+def savitzky_golay_filter(data, window_size=5, order=3):
     return savgol_filter(data, window_size, order)
 
 # Function to calculate concentration metrics based on alpha and beta correlation
-def calculate_concentration_metrics(data):
+def calculate_concentration(data):
     # Assume the data format: [Delta, Theta, Alpha, Beta, Gamma]
     delta = data[:, 0]
     theta = data[:, 1]
@@ -41,31 +45,17 @@ def calculate_concentration_metrics(data):
     gamma = data[:, 4]
     
     # Calculate correlation between alpha and beta. From 0 to 1.
-    correlation_alpha_beta = (np.corrcoef(alpha, beta)[0, 1] + 1) / 2 
+    # coherence_alpha_beta = coherence(alpha, beta)
 
     theta_beta_ratio = np.mean(beta) / np.mean(delta)
     min_value = 1.5
     max_value = 6
     theta_beta_ratio = max(0, min(1, 1 - (theta_beta_ratio - min_value) / (max_value - min_value)))
 
-
     concentartion = theta_beta_ratio
     
     return concentartion
 
-# Function to calculate concentration metrics based on alpha and beta correlation
-def calculate_concentration(data):
-    # Assume the data format: [Delta, Theta, Alpha, Beta, Gamma]
-    alpha = data[:, 2]
-    beta = data[:, 3]
-    
-    # Calculate correlation between alpha and beta
-    correlation_alpha_beta = np.corrcoef(alpha, beta)[0, 1]
-    
-    # Derive concentration metrics based on correlation (you can customize this part)
-    # concentration_metric = some_function_of_correlation(correlation_alpha_beta)
-    
-    return correlation_alpha_beta
 
 # Callback function to receive and process OSC messages
 def callback(address, *args):
@@ -73,25 +63,23 @@ def callback(address, *args):
     
     # Assume the data format: [Delta, Theta, Alpha, Beta, Gamma]
     signal = np.array(args)
-    
-    # # Denoise the signal using moving average filter
-    # denoised_signal = moving_average(signal)
 
-     # Apply Savitzky-Golay filter to denoise the signal
-    denoised_signal = savitzky_golay_filter(signal, order=3)  # Adjust order as needed
+    if len(signal_history) < WINDOW_SIZE:
+        signal_history.append(signal)
     
-    # Add the denoised signal to the history
-    signal_history.append(denoised_signal)
-    
-    # Keep only the last N=10 data signals in the history
-    signal_history = signal_history[-10:]
-    
-    # Calculate concentration metrics
-    concentration_metric = calculate_concentration(np.array(signal_history))
-    
-    # print(f"Received an OSC message at the address {address}: {args}")
-    # print(f"Denoised Signal: {denoised_signal}")
-    print(f"Concentration Metric: {concentration_metric}")
+    else:
+        signal_history = moving_average(signal_history+[signal])
+        # signal_history = savitzky_golay_filter(signal_history)
+
+        # Keep only the last data signals in the history
+        signal_history = signal_history[-WINDOW_SIZE:]
+        
+        # # Calculate concentration metrics
+        concentration_metric = calculate_concentration(np.array(signal_history))
+        
+        # print(f"Received an OSC message at the address {address}: {args}")
+        # print(f"Denoised Signal: {denoised_signal}")
+        print(f"Concentration Metric: {concentration_metric}")
 
 # Initialize OSC server and dispatcher
 dispatcher = dispatcher.Dispatcher()
